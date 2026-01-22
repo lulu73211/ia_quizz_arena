45f8e5e130e65ab15c440c997991215ca97dabc7
tcheio <thomas.cheio0105@gmail.com>
2026-01-22 15:30:17 +0100

    Presentation quizz

--- front/src/components/index.ts
+++ front/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as QuizPresenter } from "./quiz-presenter";
 export { default as QuizUser } from "./quiz-user";
 export { default as UserCrud } from "./user-crud";
 export * from "./quiz-list";
+export { default as QuizPresenterPanel } from "./quizz-presenter-pannel";
